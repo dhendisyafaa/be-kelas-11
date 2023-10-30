fa8323a9867dec3081c7156ac60a6b33bbe288f0
ZeeKotomori <136866529+ZeeKotomori@users.noreply.github.com>
2023-10-30 16:56:08 +0700

    Update VerifyToken.js / add s in header => headers

--- backend/middleware/VerifyToken.js
+++ backend/middleware/VerifyToken.js
@@ -1,7 +1,7 @@
 import  jwt, { decode }  from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-    const authHeader = req.header['authorization'];
+    const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if(token == null) return res.sendStatus(401);
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
